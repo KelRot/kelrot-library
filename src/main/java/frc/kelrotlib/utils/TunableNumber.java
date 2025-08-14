4877426cef54a34cb5fa1242b8e7f966cafa7a09
recepsirin0 <recepsirin1@outlook.com>
2025-08-14 19:29:40 +0300

    Added descriptive comments

--- src/main/java/frc/kelrotlib/utils/TunableNumber.java
+++ src/main/java/frc/kelrotlib/utils/TunableNumber.java
@@ -14,64 +14,81 @@ public class TunableNumber {
 
 
     /**
-     * @param key          
-     * @param defaultValue 
-     * @param onChange     
+     * Constructor with onChange callback
+     * @param key          The name of the tunable value
+     * @param defaultValue The default value if not set on the dashboard
+     * @param onChange     A callback that runs whenever the value changes
      */
     public TunableNumber(String key, double defaultValue, Consumer<Double> onChange) {
         this.lastValue = defaultValue;
+
+        // Get or create the NetworkTable entry for this tunable number
         entry = NetworkTableInstance.getDefault()
                 .getTable("SmartDashboard")
                 .getEntry("/Tuning" + "/" + key);
 
+        // Set the initial value on the dashboard
         entry.setDouble(defaultValue);
 
-inst.addListener(
-        entry,
-        EnumSet.of(NetworkTableEvent.Kind.kValueAll),
-        event -> {
-        double newValue = entry.getDouble(lastValue);
-          if(this.lastValue != newValue && Constants.tuningMode) {
-            DogLog.log("/Tuning" + "/" + key, entry.getDouble(lastValue));
-            System.out.println(newValue);
-            this.lastValue = newValue;
-            onChange.accept(lastValue);
-          } else { 
-            entry.setDouble(this.lastValue);
-          }
-        });
+        // Add a listener to detect changes in the entry
+        inst.addListener(
+                entry,
+                EnumSet.of(NetworkTableEvent.Kind.kValueAll),
+                event -> {
+                    double newValue = entry.getDouble(lastValue);
+
+                    // If the value changed and tuningMode is ON, accept the change
+                    if(this.lastValue != newValue && Constants.tuningMode) {
+                        DogLog.log("/Tuning" + "/" + key, entry.getDouble(lastValue));
+                        System.out.println(newValue);
+                        this.lastValue = newValue;
+                        onChange.accept(lastValue); // call the callback
+                    } else { 
+                        // Otherwise, reset the value to the last known value
+                        entry.setDouble(this.lastValue);
+                    }
+                });
     }
 
     /** 
-     * @param key          
-     * @param defaultValue 
-     * @param onChange     
+     * Constructor without onChange callback
+     * @param key          The name of the tunable value
+     * @param defaultValue The default value if not set on the dashboard
      */
     public TunableNumber(String key, double defaultValue) {
         this.lastValue = defaultValue;
+
+        // Get or create the NetworkTable entry for this tunable number
         entry = NetworkTableInstance.getDefault()
                 .getTable("SmartDashboard")
                 .getEntry("/Tuning" + "/" + key);
 
+        // Set the initial value on the dashboard
         entry.setDouble(defaultValue);
-    
-        inst.addListener(
-        entry,
-        EnumSet.of(NetworkTableEvent.Kind.kValueAll),
-        event -> {
-        double newValue = entry.getDouble(lastValue);
-          if(this.lastValue != newValue && Constants.tuningMode) {
-            DogLog.log("/Tuning" + "/" + key, entry.getDouble(lastValue));
-            System.out.println(newValue);
-            this.lastValue = newValue;
-          } else { 
-            entry.setDouble(this.lastValue);
-          }
-        });
 
+        // Add a listener to detect changes in the entry
+        inst.addListener(
+                entry,
+                EnumSet.of(NetworkTableEvent.Kind.kValueAll),
+                event -> {
+                    double newValue = entry.getDouble(lastValue);
+
+                    // If the value changed and tuningMode is ON, accept the change
+                    if(this.lastValue != newValue && Constants.tuningMode) {
+                        DogLog.log("/Tuning" + "/" + key, entry.getDouble(lastValue));
+                        System.out.println(newValue);
+                        this.lastValue = newValue;
+                    } else { 
+                        // Otherwise, reset the value to the last known value
+                        entry.setDouble(this.lastValue);
+                    }
+                });
     }
 
-    /* Return last value on the dashboard */
+    /* 
+     * Return the current value of this tunable number
+     * This always returns the last accepted value
+     */
     public double get() {
         return lastValue;
     }
