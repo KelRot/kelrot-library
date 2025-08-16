6fb20cfe33de2de5e79a985748d6f38a4acbe7eb
elma <freembot@gmail.com>
2025-08-16 21:08:31 +0300

    removed unnecessary comment lines

--- src/main/java/frc/kelrotlib/example/ExampleLibrary.java
+++ src/main/java/frc/kelrotlib/example/ExampleLibrary.java
@@ -5,12 +5,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ExampleLibrary extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   public ExampleLibrary() {
-    
   }
 
   public void exampleMethod() {
-    // Example method that could be used to control LEDs
-    // This is where you would implement your LED control logic
   }
 
   @Override
