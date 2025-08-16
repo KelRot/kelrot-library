21b504f3c7e99dfc10d3b1a879522cada5806163
elma <65625734+mekathegapple@users.noreply.github.com>
2025-08-16 20:20:55 +0300

    New Library Boilerplate (#8)
    * initial commit for library, led example
    
    * led example finished, needs review
    
    * added second group
    
    * Update Led.java
    
    * deleted Led example, added actual example files
    
    * corrected file path
    
    ---------
    
    Co-authored-by: recepsirin0 <159413980+recepsirin0@users.noreply.github.com>

---
+++ src/main/java/frc/kelrotlib/example/ExampleLibrary.java
@@ -0,0 +1,25 @@
+package frc.kelrotlib.example;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ExampleLibrary extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  public ExampleLibrary() {
+    
+  }
+
+  public void exampleMethod() {
+    // Example method that could be used to control LEDs
+    // This is where you would implement your LED control logic
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
